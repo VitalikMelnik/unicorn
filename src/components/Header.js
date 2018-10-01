ae8cdfa44edf50fae9c417e3687d1b9c9923272d
Vitalik Melnik <vitalikmelnik@Vitaliks-MacBook-Pro.local>
2018-10-01 20:03:41 +0300

    20% pr

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,67 @@ import { Link } from 'react-router-dom'
 
 // The Header creates links that can be used to navigate
 // between routes.
-const Header = () => (
-    <header>
-        <nav>
-            <ul>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/roster'>Roster</Link></li>
-                <li><Link to='/schedule'>Schedule</Link></li>
-            </ul>
-        </nav>
-    </header>
-)
+class Header extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            condition1: true,
+            condition2: false,
+            condition3: false,
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+    handleClick(i) {
+        if(i===1) {
+            this.setState({
+                condition1: !this.state.condition,
+                condition2: false,
+                condition3: false,
+            })
+        }else if(i===2){
+            this.setState({
+                condition1: false,
+                condition3: false,
+                condition2: !this.state.condition,
+            })
+        }else{
+            this.setState({
+                condition1: false,
+                condition2: false,
+                condition3: !this.state.condition,
+            })
+        }
+    }
+
+
+render(){
+    return(
+
+        <header className="sidebar">
+            <nav>
+                <ul>
+                    <li onClick={() => this.handleClick(1) }  className={ this.state.condition1 ? "item active" : "item " }><Link  to='/'>
+                        <div class="home-nav">
+
+                        </div>
+                    </Link></li>
+                    <li onClick={() => this.handleClick(2) }  className={ this.state.condition2 ? "item active" : "item" }><Link to='/roster'>
+                        <div className="blog-nav">
+
+                        </div>
+                    </Link></li>
+                    <li onClick={() => this.handleClick(3) } className={ this.state.condition3 ? "item active" : "item" }><Link to='/schedule'>
+                        <div className="person-nav">
+
+                        </div>
+                    </Link></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+}
+
 
 export default Header
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,57 @@
 import React from 'react'
 
-const Home = () => (
-    <div>
-        <h1>Welcome to the Tornadoes Website!</h1>
-    </div>
-)
+
+function getRandomArbitrary(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    var  number = Math.floor(Math.random() * (max - min + 1)) + min;
+    return '../home_wallpapers/' + number +'.jpg';
+}
+
+
+
+
+class  Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {src:(getRandomArbitrary(1,20)),}
+    }
+
+
+
+
+
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            8000
+        );
+    }
+    tick() {
+        this.setState({
+            src:(getRandomArbitrary(1,20)),
+        });
+    }
+    render(){
+        return(
+            <div class='home-div-imgbackr' style={{backgroundImage: `url(${this.state.src})`,backgroundSize:'cover',backgroundAttachment:'fixed',}}>
+                <h2 className="home-welcome-text">
+                    <p>you must</p>
+                    <div className="home-dropping-texts">
+                        <p>Work!</p>
+                        <p>Study!</p>
+                        <p>Progress!</p>
+                        <p>LIVE!</p>
+                    </div>
+                </h2>
+            </div>
+        )
+    }
+
+
+}
+
+
 
 export default Home
